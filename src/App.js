cefc3d869f5d2ddb21ac95a40bb6bc063fe27627
projectonly00000 <113275260+projectonly00000@users.noreply.github.com>
2022-11-11 20:22:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from 'react';
-
-import { About, Footer, Header, Skills, Testimonial, Work } from './container';
-import { Navbar } from './components';
 import './App.scss';
 
 const App = () => (
   <div className="app">
-    <Navbar />
-    <Header />
-    <About />
-    <Work />
-    <Skills />
-    <Testimonial />
-    <Footer />
+    <h1>this is app.js file </h1>
   </div>
 );
 
